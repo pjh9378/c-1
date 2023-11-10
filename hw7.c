964b5aba1718d25cf9752527c6aaa100fbf2d523
pjh9378 <127100406+pjh9378@users.noreply.github.com>
2023-11-10 12:52:44 +0900

    Add files via upload

---
+++ hw7.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+
+int main() {
+    int arr1[6] = { 1, 2, 3, 4, 5, 6 };
+    int arr2[6] = { 7, 8, 9, 10, 11, 12 };
+
+    printf("arr1: ");
+    for (int i = 0; i < 6; i++) {
+        printf("%d ", arr1[i]);
+    }
+    printf("\n");
+
+    printf("arr2: ");
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d ", arr2[i]);
+    }
+    printf("\n");
+
+   
+    int* ptr1 = arr1;
+    int* ptr2 = arr2;
+    for (int i = 0; i < 6; i++)
+    {
+        int temp = *ptr1;
+        *ptr1 = *ptr2;
+        *ptr2 = temp;
+        ptr1++;
+        ptr2++;
+    }
+
+    printf("after swap\n");
+
+    printf("arr1: ");
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d ", arr1[i]);
+    }
+    printf("\n");
+
+    printf("arr2: ");
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d ", arr2[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
